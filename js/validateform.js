dd23ed128abbbfcc6dcd976434be3d9722b6de22
230493131013mihir <230493131013.mihir@gmail.com>
2026-02-28 12:45:04 +0530

    uibhk

--- js/validateform.js
+++ js/validateform.js
@@ -1,5 +1,5 @@
 function validateForm() {
-//   console.log("ssdd");
+  //   console.log("ssdd");
   const name = document.contactForm.name.value;
   console.log(name);
 
@@ -12,30 +12,59 @@ function validateForm() {
   const a = document.contactForm.country.value;
   console.log(a);
 
-  if(name ==''){
-    document.getElementById("nameErr").innerHTML="please enter the name";
-  }else{
-     document.getElementById("nameErr").innerHTML="";
-  }
-   if(email ==''){
-    document.getElementById("emailErr").innerHTML="please enter the email";
-  }else{
-     document.getElementById("emailErr").innerHTML="";
-  }
-   if(mobile ==''){
-    document.getElementById("mobileErr").innerHTML="please enter the mobile number";
-  }else{
-     document.getElementById("mobileErr").innerHTML="";
-  }
+  const gender = document.contactForm.gender.value;
+  console.log(gender);
 
-  if(a =='0'){
-    document.getElementById("countryErr").innerHTML="please select the country";
-  }else{
-     document.getElementById("countryErr").innerHTML="";
+  if (name == "") {
+    document.getElementById("nameErr").innerHTML = "please enter the name";
+  } else {
+    const regexname = /^[a-zA-Z ]{2,30}$/;
+
+    if (regexname.test(name)) {
+      document.getElementById("nameErr").innerHTML = "";
+    } else {
+      document.getElementById("nameErr").innerHTML = "please enter valid name";
+    }
   }
 
+  if (email == "") {
+    document.getElementById("emailErr").innerHTML = "please enter the email";
+  } else {
+    const regexemail = /^[a-z0-9][\w\.]+\@\w+?(\.\w+){1,}$/;
 
+    if (regexemail.test(email)) {
+      document.getElementById("emailErr").innerHTML = "";
+    } else {
+      document.getElementById("emailErr").innerHTML =
+        "please enter valid email";
+    }
+  }
 
+  if (mobile == "") {
+    document.getElementById("mobileErr").innerHTML =
+      "please enter the mobile number";
+  } else {
+    const regexmobile = /^[789]\d{9}$/;
+    if (regexmobile.test(mobile)) {
+      document.getElementById("mobileErr").innerHTML = "";
+    } else {
+      document.getElementById("mobileErr").innerHTML =
+        "please enter valid phone";
+    }
+  }
+
+  if (a == "0") {
+    document.getElementById("countryErr").innerHTML =
+      "please select the country";
+  } else {
+    document.getElementById("countryErr").innerHTML = "";
+  }
+
+  if (gender == "") {
+    document.getElementById("genderErr").innerHTML = "please select gender";
+  } else {
+    document.getElementById("genderErr").innerHTML = "";
+  }
 
   return false;
 }
