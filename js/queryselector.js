0e70c7e9d21c302f1cf96fee1754298dc1efa402
230493131013mihir <230493131013.mihir@gmail.com>
2026-02-28 13:20:35 +0530

    query

---
+++ js/queryselector.js
@@ -0,0 +1,12 @@
+document.querySelector("#demo1").innerHTML="ok"
+
+document.querySelector(".main").innerHTML="this is main class"
+
+document.querySelector(".data h3").innerHTML="this is div class"
+
+
+
+function onsubmit1(){
+const name = document.querySelector("input[name='name']").value
+console.log(name)
+}
