622c59ce79465bf4d1640764e426861cdd7b61e5
230493131013mihir <230493131013.mihir@gmail.com>
2026-03-05 11:53:58 +0530

    5-3-formvalidate

--- js/validateform.js
+++ js/validateform.js
@@ -12,35 +12,34 @@ function validateForm() {
   const a = document.contactForm.country.value;
   console.log(a);
 
- let formerr = false;
+  let formerr = false;
 
   const hoobies = document.querySelectorAll('input[name="hobbies"]:checked');
   console.log(hoobies.length);
   if (hoobies.length < 2) {
     document.getElementById("hobbyErr").innerHTML =
       "please select at least 2 hoobies";
-     formerr=true;
-
+    formerr = true;
   } else {
     document.getElementById("hobbyErr").innerHTML = "";
   }
 
   if (name == "") {
     document.getElementById("nameErr").innerHTML = "please enter the name";
-    formerr=true;
+    formerr = true;
   } else {
     document.getElementById("nameErr").innerHTML = "";
   }
   if (email == "") {
     document.getElementById("emailErr").innerHTML = "please enter the email";
-    formerr=true;
+    formerr = true;
   } else {
     document.getElementById("emailErr").innerHTML = "";
   }
   if (mobile == "") {
     document.getElementById("mobileErr").innerHTML =
       "please enter the mobile number";
-      formerr=true;
+    formerr = true;
   } else {
     document.getElementById("mobileErr").innerHTML = "";
   }
@@ -48,10 +47,11 @@ function validateForm() {
   if (a == "0") {
     document.getElementById("countryErr").innerHTML =
       "please select the country";
-      formerr=true;
+    formerr = true;
   } else {
     document.getElementById("countryErr").innerHTML = "";
   }
 
+  console.log(formerr)
   return !formerr;
 }
