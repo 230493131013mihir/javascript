fdb4613363204d6346eb02e227e7be6658407d74
230493131013mihir <230493131013.mihir@gmail.com>
2026-03-05 15:05:43 +0530

    ARRAY

--- js/object.js
+++ js/object.js
@@ -7,3 +7,12 @@ const car = {
 
 // Display Data from the Object:
 document.getElementById("demo").innerHTML = "The car type is " + car.type;
+
+
+const person = {
+  firstname: "John",
+  lastname: "Doe",
+  age: 50,
+};
+
+document.getElementById("demo1").innerHTML = person.firstname + " is " + person.age;
