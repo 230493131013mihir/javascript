dbb284251d913cceccfb98811307177115e90c45
230493131013mihir <230493131013.mihir@gmail.com>
2026-03-05 12:56:52 +0530

    array

---
+++ js/array.js
@@ -0,0 +1,48 @@
+let arr = [2,4,6,8];
+
+console.log(arr[3], arr.length );
+
+
+for (let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+
+function demo(a,b){
+    console.log(a,b)
+}
+
+demo(12,14);
+
+
+const demo1 = (a,b) => {
+    console.log(a,b);
+}
+
+demo1(16,18);
+
+arr.forEach((v,i) => console.log(v,i));
+
+const r = arr.map((v, i) => {
+    console.log(v, i);
+})
+console.log(r);
+
+arr.map((v,i) => console.log(v,i))  
+
+const r1 = arr.filter((v,i) => v>5 && v<9);
+console.log(r1);
+
+const r2 = arr.reduce((acc,v,i) => acc+v ,0);
+console.log(r2);
+
+const r3 = arr.indexOf(8);
+console.log(r3);
+
+const r4 = arr.findIndex((v,i) => v>6);
+console.log(r4);
+
+const r5 = arr.find((v,i) => v>4);
+console.log(r5);
+
+const r6 = arr.includes(8);
+console.log(r6);
